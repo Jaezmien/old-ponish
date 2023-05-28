c44c07e1ccf9b15468948f4c7383004665d5413d
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2023-05-28 16:04:00 +0800

    Remake

---
+++ src/components/Dictionary.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+	import { onMounted, ref } from 'vue';
+
+	const currentLetter = ref('a');
+	const loaded = ref(false);
+
+	async function loadJSON(path: string) {
+		try {
+			const data = await fetch(path);
+
+			return await data.json();
+		} catch (err) {
+			console.error(err);
+			throw err;
+		}
+	}
+
+	onMounted(async () => {
+		// loadJSON()
+		loaded.value = true;
+	});
+</script>
+
+<template>
+	<div>
+		<div v-if="loaded">
+			<p>Huzzah!</p>
+		</div>
+		<div v-else>
+			<p>Loading the dictionary...</p>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped></style>
